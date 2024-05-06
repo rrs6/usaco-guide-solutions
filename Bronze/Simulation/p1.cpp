372f1c8b2559e989b5b767c9593265570b4d944a
rrs6 <rrs6@cin.ufpe.br>
2024-05-06 09:24:33 -0300

    [feat] adding gitignore to ignore .exe files.

---
+++ Bronze/Simulation/p1.cpp
@@ -0,0 +1,12 @@
+// Shell Game - Easy Bronze Problem.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("shell.in", "r", stdin);
+    int n;
+    cin>>n;
+    return 0;
+}
